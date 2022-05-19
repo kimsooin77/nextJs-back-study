82642232ce394a3206cba1abb5af2646a46f1593
kimsooin77 <sm6784@naver.com>
2022-05-19 17:35:45 +0900

    class문법으로 model 최신화하기

--- models/comment.js
+++ models/comment.js
@@ -1,18 +1,49 @@
-module.exports = (sequelize, DataTypes) =>  {
-    const Comment = sequelize.define('Comment', { // MySQL에는 users 테이블 생성
-        // id가 기본적으로 들어있다.
-        content : {
-            type : DataTypes.TEXT,
-            allowNull : false,
-        },
-    }, {
-        charset : 'utf8mb4',
-        collate : 'utf8mb4_general_ci', // 이모티콘 저장
-    });
-    Comment.associate = (db) => {
-        db.Comment.belongsTo(db.User);
-        db.Comment.belongsTo(db.Post);
-    };
+// 최신 문법 적용
+const DataTypes = require('sequelize');
+const { Model } = DataTypes;
 
-    return Comment;
-}
+module.exports = class Comment extends Model {
+  static init(sequelize) {
+    return super.init({
+      // id가 기본적으로 들어있다.
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      // UserId: 1
+      // PostId: 3
+    }, {
+      modelName: 'Comment',
+      tableName: 'comments',
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci', // 이모티콘 저장
+      sequelize,
+    });
+  }
+
+  static associate(db) {
+    db.Comment.belongsTo(db.User);
+    db.Comment.belongsTo(db.Post);
+  }
+};
+
+
+
+// module.exports = (sequelize, DataTypes) =>  {
+//     const Comment = sequelize.define('Comment', { // MySQL에는 users 테이블 생성
+//         // id가 기본적으로 들어있다.
+//         content : {
+//             type : DataTypes.TEXT,
+//             allowNull : false,
+//         },
+//     }, {
+//         charset : 'utf8mb4',
+//         collate : 'utf8mb4_general_ci', // 이모티콘 저장
+//     });
+//     Comment.associate = (db) => {
+//         db.Comment.belongsTo(db.User);
+//         db.Comment.belongsTo(db.Post);
+//     };
+
+//     return Comment;
+// }
